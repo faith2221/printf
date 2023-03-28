8b069c4a73eb8a33fc812ff731835d6ad36ec022
zeuss254 <ckarimi676@gmail.com>
2023-03-28 10:41:12 +0000

    update

--- main.h
+++ main.h
@@ -1,40 +1,31 @@
 #ifndef MAINN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <limits.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 /**
- * struct types_arguments - Struct op
- *
- * @t: The operator
- * @f: The function associated
+ *  * struct format - Struct for format
+ *   * @specifiers: Struct format
+ *    * @f: The function associated
  */
-typedef struct types_arguments
+typedef struct specifiers
 {
-	char t;
-	int (*f)(va_list va);
-}  types:
-int _putchar(char c);
-int _strlen(char *s);
-int print_number(va_list va);
-int binary(va_list va);
-int print_unsigned(va_list va);
-int hexa(va_list va);
-int hexa_upper(va_list va);
-int octal(va_list va);
-int print_rot(va_list va);
-char *malloc_str(char *str);
-char *rot13(char *p);
-int print_rev(va_list va);
-int hexa_special(unsigned int c);
-int stringhexa(va_list va);
-int pointer(va_list va);
-int hexa_pointer(unsigned long int c);
+	char specifiers;
+	int (*f)(va_list);
+} specifiers_t;
+
+/*prototypes*/
 int _printf(const char *format, ...);
+int get_function(char s, va_list args);
+int _putchar(char c);
+
+/*Conversion specifiers*/
+int print_char(va_list args);
+int print_string(va_list args);
+int print_digit(va_list args);
+int print_mod(va_list args);
+int print_rev_string(va_list args);
 
 #endif
-
